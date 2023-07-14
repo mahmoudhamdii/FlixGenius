a7840569648992b1288d04f23aafd66b813cc4e5
Hamdi <m.hamdi201833@gmail.co>
2023-07-14 19:52:35 +0300

    14/7

--- FlixGenius/Controllers/MainViewController/MainViewController.swift
+++ FlixGenius/Controllers/MainViewController/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  FlixGenius
 //
 //  Created by hamdi on 09/06/2023.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.title = "MAIN VIew"
     }
 
 
-}
+    
 
+}
